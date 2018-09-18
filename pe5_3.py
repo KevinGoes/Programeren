9b025ad039cf491137202e7fb6fcca2a3f79d8a3
KevinGoes <43246212+KevinGoes@users.noreply.github.com>
2018-09-18 11:20:09 +0200

    Add files via upload

---
+++ pe5_3.py
@@ -0,0 +1,5 @@
+leeftijd = int(input("Hoe oud ben je? "))
+bezitPaspoort = input("Heb je een paspoort? ")
+
+if bezitPaspoort == "ja" and leeftijd >= 18:
+    print("Gefeliciteerd!, je mag stemmen!")
