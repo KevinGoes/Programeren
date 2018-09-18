231ff03196fc3f5d2dc47564e1e12413ef272f18
Kevin Goes <kevin.goes@student.hu.nl>
2018-09-18 10:52:51 +0200

    Initial commit

---
+++ pe5_1.py
@@ -0,0 +1,10 @@
+score = int(input("Enter your Score: "))
+
+if score >= 15:
+    print("Gefeliciteerd!")
+    print("Je bent geslaagd met een score van " + str(score))
+
+else:
+    print("Helaas...")
+    print("Je bent gezakt met een score van " + str(score))
+
