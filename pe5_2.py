ff869a879fc66d73d67b656fbff4906b0188383f
Kevin Goes <kevin.goes@student.hu.nl>
2018-09-18 11:01:26 +0200

    pe5_2

---
+++ pe5_2.py
@@ -0,0 +1,8 @@
+leeftijd = int(input("Hoe oud ben je? "))
+bezitPaspoort = input("Heb je een paspoort? ")
+
+if bezitPaspoort == "ja" and leeftijd >= 18:
+    print("Gefeliciteerd!, je mag stemmen!")
+
+else:
+    print("Helaas je mag niet stemmen")
