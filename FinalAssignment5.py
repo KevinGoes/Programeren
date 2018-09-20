e3c26c01e153a753d1355d0bbd15386d3ba550a2
Kevin Goes <kevin.goes@student.hu.nl>
2018-09-20 09:48:51 +0200

    Final Assignment Les 5

--- FinalAssignment5.py
+++ FinalAssignment5.py
@@ -6,24 +6,32 @@ if kilometer > 50:
     standaardtarief = (15+0.60*kilometer)
     if tijdWeek == 'ja':
         if leeftijd <= 12 or leeftijd >= 65:
+            print("U krijgt 35% korting, daarmee komt u uit op een bedrag van: ")
             print(standaardtarief*0.65)
         else:
+            print("U krijgt 40% korting, daarmee komt u uit op een bedrag van: ")
             print(standaardtarief*0.60)
     else:
         if leeftijd <= 12 or leeftijd >= 65:
+            print("U krijgt 30% korting, daarmee komt u uit op een bedrag van: ")
             print(standaardtarief*0.7)
         else:
+            print("Helaas, u krijgt geen korting, daarmee komt u uit op een bedrag van: ")
             print(standaardtarief)
 else:
     standaardtarief = (0.80*kilometer)
     if tijdWeek == 'ja':
         if leeftijd <= 12 or leeftijd >= 65:
+            print("U krijgt 35% korting, daarmee komt u uit op een bedrag van: ")
             print(standaardtarief * 0.65)
         else:
+            print("U krijgt 40% korting, daarmee komt u uit op een bedrag van: ")
             print(standaardtarief * 0.60)
     else:
         if leeftijd <= 12 or leeftijd >= 65:
+            print("U krijgt 30% korting, daarmee komt u uit op een bedrag van: ")
             print(standaardtarief * 0.7)
         else:
+            print("Helaas, u krijgt geen korting, daarmee komt u uit op een bedrag van: ")
             print(standaardtarief)
 
