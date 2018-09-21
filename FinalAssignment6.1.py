3cf872171a114b189f1507ca2e71f04a1355db57
Kevin Goes <kevin.goes@student.hu.nl>
2018-09-21 15:33:54 +0200

    Final Assignment Les 6, Kevin Goes

---
+++ FinalAssignment6.1.py
@@ -0,0 +1,40 @@
+kilometer = int(input("Hoeveel kilometer moet u reizen? "))
+leeftijd = int(input("Hoe oud bent u? "))
+tijdWeek = str(input("Reist u in het weekend? 'ja' of 'nee' "))
+
+if kilometer > 50:
+    standaardtarief = (15+0.60*kilometer)
+    if tijdWeek == 'ja':
+        if leeftijd <= 12 or leeftijd >= 65:
+            print("U krijgt 35% korting, daarmee komt u uit op een bedrag van: ")
+            print(round(standaardtarief*0.65, 2))
+        else:
+            print("U krijgt 40% korting, daarmee komt u uit op een bedrag van: ")
+            print(round(standaardtarief*0.60, 2))
+    if tijdWeek == 'nee':
+        if leeftijd <= 12 or leeftijd >= 65:
+            print("U krijgt 30% korting, daarmee komt u uit op een bedrag van: ")
+            print(round(standaardtarief*0.7, 2))
+        else:
+            print("Helaas, u krijgt geen korting, daarmee komt u uit op een bedrag van: ")
+            print(round(standaardtarief, 2))
+    else:
+        print(tijdWeek + " is geen ja of nee")
+else:
+    standaardtarief = (0.80*kilometer)
+    if tijdWeek == 'ja':
+        if leeftijd <= 12 or leeftijd >= 65:
+            print("U krijgt 35% korting, daarmee komt u uit op een bedrag van: ")
+            print(round(standaardtarief * 0.65, 2))
+        else:
+            print("U krijgt 40% korting, daarmee komt u uit op een bedrag van: ")
+            print(round(standaardtarief * 0.60, 2))
+    if tijdWeek == 'nee':
+        if leeftijd <= 12 or leeftijd >= 65:
+            print("U krijgt 30% korting, daarmee komt u uit op een bedrag van: ")
+            print(round(standaardtarief * 0.7, 2))
+        else:
+            print("Helaas, u krijgt geen korting, daarmee komt u uit op een bedrag van: ")
+            print(round(standaardtarief, 2))
+    else:
+        print(tijdWeek + " is geen ja of nee")
--- FinalAssignment6.py
+++ FinalAssignment6.py
@@ -1,40 +1,25 @@
-kilometer = int(input("Hoeveel kilometer moet u reizen? "))
+afstandKM = int(input("Hoeveel Kilometer moet u reizen? "))
 leeftijd = int(input("Hoe oud bent u? "))
-tijdWeek = str(input("Reist u in het weekend? 'ja' of 'nee' "))
+weekendrit = input("Reist u in het weekend 'ja' of 'nee'? ")
 
-if kilometer > 50:
-    standaardtarief = (15+0.60*kilometer)
-    if tijdWeek == 'ja':
-        if leeftijd <= 12 or leeftijd >= 65:
-            print("U krijgt 35% korting, daarmee komt u uit op een bedrag van: ")
-            print(round(standaardtarief*0.65, 2))
-        else:
-            print("U krijgt 40% korting, daarmee komt u uit op een bedrag van: ")
-            print(round(standaardtarief*0.60, 2))
-    if tijdWeek == 'nee':
-        if leeftijd <= 12 or leeftijd >= 65:
-            print("U krijgt 30% korting, daarmee komt u uit op een bedrag van: ")
-            print(round(standaardtarief*0.7, 2))
-        else:
-            print("Helaas, u krijgt geen korting, daarmee komt u uit op een bedrag van: ")
-            print(round(standaardtarief, 2))
+def standaardprijs(afstandKM):
+    if afstandKM > 50:
+        return afstandKM * 0.60 + 15
     else:
-        print(tijdWeek + " is geen ja of nee")
-else:
-    standaardtarief = (0.80*kilometer)
-    if tijdWeek == 'ja':
-        if leeftijd <= 12 or leeftijd >= 65:
-            print("U krijgt 35% korting, daarmee komt u uit op een bedrag van: ")
-            print(round(standaardtarief * 0.65, 2))
+        return afstandKM * 0.80
+
+def ritprijs(leeftijd, weekendrit, afstandKM):
+    if weekendrit == 'ja':
+        if leeftijd < 12 or leeftijd >= 65:
+            return round(afstandKM * 0.65, 2)
         else:
-            print("U krijgt 40% korting, daarmee komt u uit op een bedrag van: ")
-            print(round(standaardtarief * 0.60, 2))
-    if tijdWeek == 'nee':
-        if leeftijd <= 12 or leeftijd >= 65:
-            print("U krijgt 30% korting, daarmee komt u uit op een bedrag van: ")
-            print(round(standaardtarief * 0.7, 2))
+            return round(afstandKM * 0.60, 2)
+    if weekendrit == 'nee':
+        if leeftijd < 12:
+            return round(afstandKM * 0.60, 2)
         else:
-            print("Helaas, u krijgt geen korting, daarmee komt u uit op een bedrag van: ")
-            print(round(standaardtarief, 2))
+            return round(afstandKM, 2)
     else:
-        print(tijdWeek + " is geen ja of nee")
+        print(weekendrit + " is geen ja of nee!")
+
+print(ritprijs(leeftijd, weekendrit, afstandKM))
