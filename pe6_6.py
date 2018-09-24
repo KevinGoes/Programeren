11d5d7e178bd89db46448b401cc727f45131201c
Kevin Goes <kevin.goes@student.hu.nl>
2018-09-24 11:46:36 +0200

    FinalAssignment 6 update loop, white line

---
+++ pe6_6.py
@@ -0,0 +1,12 @@
+def wijzig(letterlijst):
+    lijst = letterlijst
+    letterlijst[0] = 'd'
+    letterlijst[1] = 'e'
+    letterlijst[2] = 'f'
+
+
+lijst = ['a', 'b', 'c']
+
+print(lijst)
+wijzig(lijst)
+print(lijst)
