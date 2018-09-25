7e28824dadb19238d890097fa0aeb79fa7234228
Kevin Goes <kevin.goes@student.hu.nl>
2018-09-25 15:12:02 +0200

    pe7_4

---
+++ pe7_4.py
@@ -0,0 +1,12 @@
+import datetime
+vandaag = datetime.datetime.today()
+s = vandaag.strftime("%a %d %b %Y, %I:%M:%S")
+laatsteBinnenkomer = ' '
+binnenKomer = input("Hoe heet je? ")
+
+while binnenKomer != laatsteBinnenkomer:
+    invoer = open('hardlopers.txt', 'a')
+    invoer.write(s + ' ' + binnenKomer + '\n')
+    laatsteBinnenkomer = binnenKomer
+    invoer.close()
+    binnenKomer = input("Hoe heet je? ")
