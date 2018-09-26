a3d207afd497675ddd4de78dc12de32d1a9ffbbb
Kevin Goes <kevin.goes@student.hu.nl>
2018-09-26 10:13:15 +0200

    pe7_1

--- pe7_1.py
+++ pe7_1.py
@@ -1,12 +1,16 @@
-def covert(tempC):
+def convert(tempC):
     tempF = tempC * 1.8 + 32
     return tempF
 
-# def table(tempF):
-#    for tempF in tempCe:
-#        return tempF
+def table():
+    print("F \t \t C")
+    for i in range(-30, 41, 10):
+        uitkomst = i * 1.8 + 32
+        print(uitkomst, "\t", float(i))
 
 
-
+print(convert(25))
+print("\n")
+print(table())
 
 
