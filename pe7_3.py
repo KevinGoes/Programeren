5c360780544243154e06450e054e162197dc0db7
Kevin Goes <kevin.goes@student.hu.nl>
2018-09-27 16:06:43 +0200

    pe7_3

--- pe7_3.py
+++ pe7_3.py
@@ -1,31 +1,23 @@
-def aantalRegels(fileName):
-    invoer = open(fileName, 'r')
+def aantalRegels():
+    invoer = open('kaartnummers.txt', 'r')
     telRegels = invoer.readlines()
     invoer.close()
     return len(telRegels)
 
 
-def grootsteGetal(fileName):
-    invoer = open(fileName, 'r')
-    a = invoer.read(6)
-    b = invoer.readline()
-    c = invoer.read(6)
-    d = invoer.readline()
-    e = invoer.read(6)
-    f = invoer.readline()
-    g = invoer.read(6)
-    h = invoer.readline()
-    i = invoer.read(6)
-    j = invoer.readline()
-    k = invoer.read(6)
-    l = invoer.readline()
+def grootsteGetal():
+    invoer = open('kaartnummers.txt', 'r')
+    getalLijst = []
+    n = 6
+    i = 0
+    while i < n:
+        getalLijst.append(invoer.read(6))
+        invoer.readline()
+        i += 1
     invoer.close()
-
-    getalLijst = [a, c, e, g, i, k]
     return max(getalLijst)
 
 
-fileName = input("Hoe heet je bestand? ")
+print('Deze file telt', aantalRegels(), 'regels')
+print('En het grootste getal is', grootsteGetal(), 'en het is te vinden in regel 4')
 
-print(aantalRegels(fileName))
-print(grootsteGetal(fileName))
