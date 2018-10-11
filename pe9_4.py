d6852c812a0d8535f4ec17d9b079f644c41e830b
Kevin Goes <kevin.goes@student.hu.nl>
2018-10-11 10:03:14 +0200

    pe9

--- pe9_4.py
+++ pe9_4.py
@@ -1,2 +1,23 @@
-def ticker(fileName):
-    return
+def ticker():
+    dict = {}
+    fileInvoer = open('ticker.txt', 'r')
+    aantalRegels = fileInvoer.readlines()
+    fileInvoer.seek(0)
+    for regel in range(len(aantalRegels)):
+        tmp = fileInvoer.readline()
+        tmp = tmp.replace('\n', '')
+        tmp = tmp.split(':')
+        dict[tmp[0]] = tmp[1]
+    fileInvoer.close()
+    return dict
+
+
+dict = ticker()
+invoerCompany = input('Enter Company Name: ')
+print('Ticker Symbol = {} \n'.format(dict[invoerCompany]))
+
+invoerAfkorting = input('Enter Ticker Symbol: ')
+for regel in dict:
+    if dict[regel] == invoerAfkorting:
+        print('Company Name = {}'.format(regel))
+
